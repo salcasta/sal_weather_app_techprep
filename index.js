2936b84c76e0e9591638de333727929b2c2a27ea
salcasta <salcasta@gmail.com>
2023-03-19 17:50:30 -0500

    background image will change based on the time. Project complete

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function displayResults(weather) {
 
 function dateBuilder(d) {
     let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
     let day = days[d.getDay()];
     let date = d.getDate();
@@ -48,4 +48,12 @@ function dateBuilder(d) {
     let year = d.getFullYear();
 
     return `${day} ${date} ${month} ${year}`
+}
+
+//background image will change based on the time of day
+let now = new Date();
+if (now.getHours() > 6 && now.getHours() < 20) {
+    document.body.className += " day";
+} else {
+    document.body.className += " night";
 }
